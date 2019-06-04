90ade9cac7f34c8d83c54d01da6bb7c597902b18
Typicalstudent <50824591+Typicalstudent@users.noreply.github.com>
2019-06-04 18:45:50 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import paper.OfficePaper;
-import paper.Paper;
 import paper.PhotoPaper;
 
 public class Main {
     public static void main(String[] args) {
         Human human = new Human();
-        Printer printer =  new Printer();
-        printer.printText((OfficePaper)human.PaperFill(0));
-        printer.printImage((PhotoPaper)human.PaperFill(1));
+        Printer printer = new Printer();
+        /* if button "print text" is pressed, uses printText func.
+            stock parameters of text size is 600x400
+         */
+        printer.printText((OfficePaper) human.PaperFill(0), 600, 400);
+
+        /* if button "print image" is pressed, uses printImage func.
+            stock parameters of text size is 600x400
+        */
+        printer.printImage((PhotoPaper) human.PaperFill(1), 600, 400);
     }
 }
