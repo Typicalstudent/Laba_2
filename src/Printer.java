b6ac14f24d36ead7fa7e8057b6e4e5d8543e464d
Typicalstudent <50824591+Typicalstudent@users.noreply.github.com>
2019-06-04 18:44:31 +0300

    Update Printer.java

--- src/Printer.java
+++ src/Printer.java
@@ -2,12 +2,16 @@ import paper.*;
 
 public class Printer {
 
-    public void printText(OfficePaper paper) {
+    public void printText(OfficePaper paper, int newLen, int newHig) {
         System.out.println("printing text" );
         paper.used();
+        paper.setLen(newLen);
+        paper.setHig(newHig);
     }
-    public void printImage(PhotoPaper paper) {
+    public void printImage(PhotoPaper paper, int newLen, int newHig) {
         System.out.println("printing image");
         paper.used();
+        paper.setLen(newLen);
+        paper.setHig(newHig);
     }
 }
